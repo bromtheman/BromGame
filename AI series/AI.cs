17d9183d31d6556c7a60bdc050e9ca5f768755d4
bromtheman <JFinn715@gmail.com>
2017-02-11 13:00:05 -0600

    Add files via upload

---
+++ AI series/AI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private UserPlayer UserPlayer;
+    private List<Vector3> selection;
+    private MasterDirector MasterDirector;
+    float smallestDistance = -1;
+    int closestPlayer = -1;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < MasterDirector.Players.Count; i++)
+        {
+            float distance = Vector3.Distance(MasterDirector.Players[i].transform.position, transform.position);
+            if (distance < smallestDistance | smallestDistance == -1)
+            {
+                smallestDistance = distance;
+                closestPlayer = i;
+            }
+        }
+
+        if (closestPlayer != -1)
+        {
+            var TheClosestPlayer = MasterDirector.Players[closestPlayer];
+            agent.SetDestination(TheClosestPlayer.gameObject.transform.position);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            for (int i = 0; i < MasterDirector.Players.Count; i++)
+            {
+                print(MasterDirector.Players[i]);
+            }
+
+        }
+    }
+}
---
+++ AI series/MasterDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterDirector : MonoBehaviour
+{
+    public static List<GameObject> Players;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Players = new List<GameObject>();
+        Players.AddRange(GameObject.FindGameObjectsWithTag("Target"));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PlayerDeath()
+    {
+    }
+}
