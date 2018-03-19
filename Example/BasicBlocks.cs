4b12152cee9b125503e432507168f81a97ca37c9
bromtheman <JFinn715@gmail.com>
2018-03-19 09:12:59 -0500

    Add files via upload

---
+++ Example/BasicBlocks.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBlocks : MonoBehaviour
+{
+    public int Health = 0;
+    public int Mass = 0;    
+    private int MaxHealth = 0;
+    private int StartingMass;
+    
+
+    public void Start()
+    {
+
+        MaxHealth = Health;
+        StartingMass = Mass;
+    }
+
+
+
+    protected void TakeDamage(int Damage)
+    {
+        print(Health);
+        Health = Health - Damage;
+        CheckForDeath();
+    }
+    void CheckForDeath()
+    {
+        if(Health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+
+}
+
+
+
---
+++ Example/BasicShell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicShell : MonoBehaviour {
+
+
+    public int shellDamage = 10;
+    public float speed = 5f;
+            
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 forward = new Vector2(0, speed) * Time.deltaTime;
+        transform.Translate(forward);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        print("I hit: " + collision.gameObject.name);
+        SendMessageUpwards("base.TakeDamage", shellDamage, SendMessageOptions.DontRequireReceiver);
+
+
+    }
+
+
+}
